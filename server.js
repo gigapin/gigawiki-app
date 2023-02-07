d07f7d40af6498c06233c371f8f91717e07a28d7
Giuseppe Galari <49388080+gigapin@users.noreply.github.com>
2023-02-07 14:50:55 +0100

    Add Dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+require('dotenv').config()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')))
